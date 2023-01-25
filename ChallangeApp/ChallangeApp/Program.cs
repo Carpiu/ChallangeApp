3036f2462b1ae5dcab1f9198e5c84127a9bd11aa
Kacper Karpiński <karpinkacper@gmail.com>
2023-01-25 20:24:20 +0100

    zadanie domowe dzień

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,52 +1,69 @@
-﻿var name = ("Ewa");
-var gender = ("female");
-var age = 33;
+﻿
+using System.Diagnostics.Metrics;
 
-if (gender == "female" && age > 30)
+int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+
+int[] counter = new int[10];
+counter[0] = 0;
+counter[1] = 0;
+counter[2] = 0;
+counter[3] = 0;
+counter[4] = 0;
+counter[5] = 0;
+counter[6] = 0;
+counter[7] = 0;
+counter[8] = 0;
+counter[9] = 0;
+
+
+foreach (char letter in letters)
 {
-
-    Console.WriteLine("Kobieta powyżej 30 lat");
-
+    if (letter == '0')
+    {
+        counter[0]++;
+    }
+    else if (letter == '1')
+    {
+        counter[1]++;
+    }
+    else if (letter == '2')
+    {
+        counter[2]++;
+    }
+    else if (letter == '3')
+    {
+        counter[3]++;
+    }
+    else if (letter == '4')
+    {
+        counter[4]++;
+    }
+    else if (letter == '5')
+    {
+        counter[5]++;
+    }
+    else if (letter == '6')
+    {
+        counter[6]++;
+    }
+    else if (letter == '7')
+    {
+        counter[7]++;
+    }
+    else if (letter == '8')
+    {
+        counter[8]++;
+    }
+    else
+    {
+        counter[9]++;
+    }
 }
 
-else if (gender == "female" && age < 30)
-{
+for (int i =0; i<10; i++)
+    {
 
-    Console.WriteLine("kobieta poniżej 30 lat");
-
-}
-
-else
-{
-    Console.WriteLine("Nie jest to kobieta");
-}
-
-// 2 -----------------------------------
-if (name == "Ewa" && age == 33)
-{
-
-    Console.WriteLine("Ewa, lat 33");
-
-}
-
-else
-{
-
-    Console.WriteLine("Nie jest to 33 letnia Ewa");
-
-}
-
-// 3 --------------------------------------
-if (age > 18 && gender == "female")
-{
-
-    Console.WriteLine("Pełnoletnia Kobieta");
-
-}
-
-else
-{
-
-    Console.WriteLine("Niepełnoletnia Kobieta");
-
-}
+    Console.WriteLine(i + "=>" + counter[i]);
+    }
